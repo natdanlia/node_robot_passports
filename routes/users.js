c81c64f7ed78172e31bf54e9c9653b8af19cf66b
Nathan Gezahegne <nathan.daniel5719@gmail.com>
2017-08-04 10:43:24 -0400

    working on edit pages currently, add page is finished

--- routes/users.js
+++ routes/users.js
@@ -6,19 +6,32 @@ router.get("/add", (req, res) => {
   res.render("add");
 });
 
+router.get("/edit", (req, res) => {
+  res.render("edit");
+})
+
 router.post("/new", (req, res) => {
   console.log(req.body);
-  new User(req.body).save().then(function(newUser) {
+  new User(req.body).save().then((newUser) => {
     console.log(arguments);
     res.redirect(`/users/${newUser._id}`)
     // res.render(req.body);
-  }).catch(function(err){
+  }).catch((err) => {
     console.log(arguments);
     res.send("OOPS")
   });
 });
 
-router.get("/:id", function(req, res) {
+router.post("/edit", (req, res) => {
+  User.findOne({_id: req.params.id})
+    .then((data) => {
+      res.render("edit", {users: data})
+    }).catch((err) => {
+      res.send("oops")
+    })
+})
+
+router.get("/:id", (req, res) => {
   console.log(req.params);
   User.findOne({ _id: req.params.id }).then(data => {
     res.render("profile", data);
