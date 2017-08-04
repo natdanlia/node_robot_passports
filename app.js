63fa1e8e3ced5ba7378bde01f7a91533f043e73d
Nathan Gezahegne <nathan.daniel5719@gmail.com>
2017-08-04 12:12:34 -0400

    finished edit page. moving on to delete page

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.static(path.join(__dirname, "public")))
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(indexRoute)
-app.use("/users/", require("./routes/users"))
+app.use("/users", require("./routes/users"))
 
 
 // Start a db connect and list after it's connected.
--- routes/users.js
+++ routes/users.js
@@ -6,9 +6,7 @@ router.get("/add", (req, res) => {
   res.render("add");
 });
 
-router.get("/edit", (req, res) => {
-  res.render("edit");
-})
+
 
 router.post("/new", (req, res) => {
   console.log(req.body);
@@ -22,20 +20,35 @@ router.post("/new", (req, res) => {
   });
 });
 
-router.post("/edit", (req, res) => {
-  User.findOne({_id: req.params.id})
-    .then((data) => {
-      res.render("edit", {users: data})
-    }).catch((err) => {
-      res.send("oops")
-    })
-})
+
 
 router.get("/:id", (req, res) => {
-  console.log(req.params);
+  // console.log(req.params);
   User.findOne({ _id: req.params.id }).then(data => {
     res.render("profile", data);
   });
 });
 
+router.get("/:id/edit", (req, res) => {
+  User.findOne({ _id: req.params.id })
+  .then((data) => {
+    res.render("edit", {users: data})
+  });
+});
+
+router.post("/:id/edit", (req, res) => {
+  User.findByIdAndUpdate( { _id: req.params.id }, {
+    company: req.body.company,
+    email: req.body.email,
+    job: req.body.job,
+    name: req.body.name,
+    phone: req.body.phone,
+    username: req.body.username,
+    password: req.body.password
+  }) .then(() => {
+    res.redirect(".")
+  }).catch((err) => {
+    res.send("oops")
+  })
+})
 module.exports = router;
